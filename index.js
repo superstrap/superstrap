77941163ff764fede0bd5f179b622a004e6bcb4b
gtoy1118 <106995145+gtoy1118@users.noreply.github.com>
2023-04-25 00:08:52 +0000

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+// NOTE: DON'T USE THIS FOR ACTUAL SERVERS WITH BACKEND!
+// HAVE ACTUAL PATHS!
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/views"), (_, res, next) => {
+  res.status(404)
+  res.sendFile(__dirname + "/404.html")
+});
+
+app.listen(8080);
